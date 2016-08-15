1287804546c24becd33b06728a3d103ed43a397d
Josh <mcnett88@gmail.com>
2016-08-15 16:50:45 -0400

    initial commit

---
+++ coin_changer.rb
@@ -0,0 +1,8 @@
+# coin changer kata
+
+# write a function that takes a number of cents, and then returns a hash containing the least number of coins needed to generate the value of cents.
+
+eg. if you have 11 cents you should return {:dime => 1, :penny =>1} if you have 31 cents you should return {:quater => 1, :nickel => 1, :penny =>1"}; if 93 cents {:quarter => 3, :dime => 1, nickel =>1, penny: => 3}
+
+build your coin_changer using test. commit to git after every test.
+
