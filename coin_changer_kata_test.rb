a77a3b1ed573e02fa6a89f1df2ff5f2af231a465
Josh <mcnett88@gmail.com>
2016-08-17 14:08:20 -0400

    second test passes for 1 nickle and 1 dime for 15

--- coin_changer_kata_test.rb
+++ coin_changer_kata_test.rb
@@ -4,7 +4,11 @@ require "minitest/autorun"
 class Coin_changer<Minitest::Test
 
 	def test_for_nickle_to_return_1
-	assert_equal({:quarter => 0, :dime => 0, :nickle => 1, :penny => 0}, money(5))
+		assert_equal({:quarter => 0, :dime => 0, :nickle => 1, :penny => 0}, money(5))
+	end
+
+	def test_for_1_nickle_and_1_dime
+		assert_equal({:quarter => 0, :dime => 1, :nickle => 1, :penny => 0}, money(15))
 	end
 
 end
