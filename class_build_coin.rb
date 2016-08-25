609cf7c963febc50f97ba2b025c54e3f50dc34aa
Josh <mcnett88@gmail.com>
2016-08-25 11:52:21 -0400

    refactored

--- class_build_coin.rb
+++ class_build_coin.rb
@@ -1,52 +1,15 @@
 def generate_change(cents)
-
-	# coins_value = {:quarter => 25, :dime => 10, :nickel => 5, :penny => 1}
 	change = {}
-		# coins_value.each do |coin, value|
-			
-		# 	if 
-		# 		value <= cents
-		# 	then
-		# 		change[coin] = 0
-		# 	end
 
+		coins_value= {:quarter => 25, :dime => 10, :nickel => 5, :penny => 1}
 
-
-		# 	while
-		# 		amount = cents / value
-		# 		change[coin] = amount
-		# 		cents = cents - (value * amount)
-		# 	end
-		# end
-		quarter = 25
-		dime = 10
-		nickel = 5
-		penny = 1
+		coins_value.each do |coin_name, coin_value|
 		
-		if cents >= quarter
-			coins = cents / quarter
-			change[:quarter] = coins
-			cents = cents - (quarter * coins)
+		if cents >= coin_value
+			coins = cents / coin_value
+			change[coin_name] = coins
+			cents = cents - (coin_value * coins)
 		end
-
-		if cents >= dime
-			coins = cents / dime
-			change[:dime] = coins
-			cents = cents - (dime * coins)
-		end
-
-		if cents >= nickel
-			coins = cents / nickel
-			change[:nickel] = coins
-			cents = cents - nickel
-		end
-		
-		if cents >= penny
-			coins = cents / penny
-			change[:penny] = coins
-			cents = cents - penny
-		end
-
-
-		change
+	end
+change
 end
