47822408413e8948ec976326375a6cfe9b29e8ca
Josh <mcnett88@gmail.com>
2016-08-25 11:15:03 -0400

    refactored

--- class_build_coin.rb
+++ class_build_coin.rb
@@ -1,55 +1,48 @@
 def generate_change(cents)
 
-	coins_value = {:quarter => 25, :dime => 10, :nickel => 5, :penny => 1}
+	# coins_value = {:quarter => 25, :dime => 10, :nickel => 5, :penny => 1}
 	change = {}
-		
-
-
-
-		coins_value.each do |coin, value|
+		# coins_value.each do |coin, value|
 			
-			if 
-				value <= cents
-			then
-				change[coin] = 0
-			end
+		# 	if 
+		# 		value <= cents
+		# 	then
+		# 		change[coin] = 0
+		# 	end
 
 
 
-			while
-				amount = cents / value
-				change[coin] = amount
-				cents = cents - (value * amount)
-			end
+		# 	while
+		# 		amount = cents / value
+		# 		change[coin] = amount
+		# 		cents = cents - (value * amount)
+		# 	end
+		# end
+		quarter = 25
+		dime = 10
+		nickel = 5
+		penny = 1
+		
+		if cents >= quarter
+			coins = cents / quarter
+			change[:quarter] = coins
+			cents = cents - (quarter * coins)
 		end
 
+		if cents >= dime
+			coins = cents / dime
+			change[:dime] = coins
+			cents = cents - (dime * coins)
+		end
 
-
-
-
-
-
-
-		# if cents >= 25
-		# 	coins = cents / 25
-		# 	change[:quarter] = coins
-		# 	cents = cents - (25 * coins)
-		# end
-
-		# if cents >= 10
-		# 	coins = cents / 10
-		# 	change[:dime] = coins
-		# 	cents = cents - (10 * coins)
-		# end
-
-		# if cents >= 5
-		# 	change[:nickel] = cents / nickel_value
-		# 	cents = cents - 5
-		# end
+		if cents >= nickel
+			change[:nickel] = cents / nickel
+			cents = cents - nickel
+		end
 		
-		# if cents > 0
-		# 	change[:penny] = cents
-		# end
+		if cents > 0
+			change[:penny] = cents
+		end
 
 
 		change
